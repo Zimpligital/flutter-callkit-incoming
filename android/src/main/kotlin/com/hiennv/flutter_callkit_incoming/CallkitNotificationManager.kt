be30ef760653db2a9b56061206510c9e8ea39c0f
Natdanai Janthakool <natdanai@zimpligital.com>
2025-07-16 09:45:57 +0700

    fix(call): update incoming notification clearing to use new intent for ended calls

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -849,12 +849,7 @@ class CallkitNotificationManager(
     fun clearIncomingNotification(data: Bundle, isAccepted: Boolean) {
         callkitSoundPlayerManager?.stop()
 
-        context?.sendBroadcast(
-            CallkitIncomingBroadcastReceiver.getIntentTimeout(
-                requireNotNull(context),
-                data
-            )
-        )
+        context?.sendBroadcast(CallkitIncomingActivity.getIntentEnded(context, isAccepted))
         val notificationId =
             data.getString(CallkitConstants.EXTRA_CALLKIT_ID, "callkit_incoming").hashCode()
         getNotificationManager().cancel(notificationId)
