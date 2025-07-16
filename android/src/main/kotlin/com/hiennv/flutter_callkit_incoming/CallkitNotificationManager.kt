c844c20e5d460451a8de4a4a2579214fec451e4c
Natdanai Janthakool <natdanai@zimpligital.com>
2025-07-16 09:32:46 +0700

    fix(call): ensure context is not null when sending broadcast for incoming notification timeout

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -849,7 +849,12 @@ class CallkitNotificationManager(
     fun clearIncomingNotification(data: Bundle, isAccepted: Boolean) {
         callkitSoundPlayerManager?.stop()
 
-//        context.sendBroadcast(CallkitIncomingBroadcastReceiver.getIntentTimeout(this@CallkitIncomingActivity, data))
+        context?.sendBroadcast(
+            CallkitIncomingBroadcastReceiver.getIntentTimeout(
+                requireNotNull(context),
+                data
+            )
+        )
         val notificationId =
             data.getString(CallkitConstants.EXTRA_CALLKIT_ID, "callkit_incoming").hashCode()
         getNotificationManager().cancel(notificationId)
